5362b2704d95e6c72f39b66f254993807ef93750
“Akinsefunmi <Bolytolexayus@gmail.com>
2023-02-21 19:15:50 -0800

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,6 +10,7 @@
 
 int print_sign(int n)
 {
+
 	if (n > 0)
 	{
 		_putchar('+');
@@ -17,7 +18,7 @@ int print_sign(int n)
 		_putchar(' ');
 		return (1);
 	}
-	else if ('n -- 0')
+	else if (n - 0)
 	{
 		_putchar('0');
 		_putchar(',');
@@ -29,6 +30,8 @@ int print_sign(int n)
 		_putchar('-');
 		_putchar(',');
 		_putchar(' ');
+		return (-1);
 	}
+
 }
 
