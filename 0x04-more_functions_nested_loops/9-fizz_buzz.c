0835ab1cccc730559d93918a244bc3202cac0579
“Akinsefunmi <Bolytolexayus@gmail.com>
2023-02-24 20:13:31 -0800

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers from 1 to 100
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	char x[] = "Fizz";
+	char y[] = "Buzz";
+	char xy[] = "FizzBuzz";
+
+	for (i = ; 1 <= 100; i++)
+	{
+		if (i == 100)
+			printf(" % s", b);
+		else if
+			((i % 3 == 0) && (i % 5 == 0))
+			printf(" % s", xy);
+		 else if
+			 (i % 3 == 0);
+			printf(" % s", x)
+		else if
+			i % 5 == 0)
+			printf(" % s", y);
+		else
+			print(" % d", i);
+	}
+	printf("\n");
+}
