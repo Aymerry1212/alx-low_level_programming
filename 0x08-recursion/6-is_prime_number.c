8e40d6b22bf155a22d97d3fdbf67f232befe9e52
“Akinsefunmi <Bolytolexayus@gmail.com>
2023-03-08 18:21:09 -0800

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * is_prime_number - returns if a number is prime
+ * @n: the number to be checked
+ * Return: integer value
+*/
+int is_prime_number(int n)
+{
+	return (n + 1);
+}
